89bf5b560fa7e690f40d90f71e2f6be3f706d851
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-10-13 11:23:05 -0600

    Single line bug fix

--- jsion.ts
+++ jsion.ts
@@ -1,5 +1,5 @@
 namespace JSION {
-    const COMMENT_PATTERN = /(?<=\\"|"(?:\\"|[^"])*")|(?:#(?:\\"|[^"\\\n])*)/gm;
+    const COMMENT_PATTERN = /(?<=\\"|"(?:\\"|[^"])*")|(?:#(?:\\"|\\\\|\\\[|\\]|\\{|\\}|\\,|\\:|[^"\\\n\[\]{},:])*)/gm;
     export function parse(text: string, reviver?: ((this: any, key: string, value: any) => any) | undefined): any {
         return JSON.parse(text.replace(COMMENT_PATTERN, ''), reviver);
     }
