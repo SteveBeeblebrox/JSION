6fc62791c93c604dfe0a4179de770f4199a95ce5
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-10-16 18:23:39 -0600

    Update jsion.js

--- jsion.js
+++ jsion.js
@@ -1,9 +1,13 @@
 "use strict";
 var JSION;
 (function (JSION) {
-    const COMMENT_PATTERN = /(?<=\\"|"(?:\\"|[^"])*")|(?:#(?:\\["\\\[\]{},:]|[^"\\\n\[\]{},:])*)/gm;
+    const COMMENT_PATTERN = /\\"|"(?:\\"|[^"])*"|(#[\s\S]*?)(?=(?<!\\|\\\d+)(?:true|false|null|\d+|[",:{}[\]]))/g;
     function parse(text, reviver) {
-        return JSON.parse(text.replace(COMMENT_PATTERN, ''), reviver);
+        return JSON.parse(text.replace(COMMENT_PATTERN, function (substring, ...args) {
+            if (!args[0])
+                return substring;
+            return '';
+        }), reviver);
     }
     JSION.parse = parse;
     JSION.stringify = JSON.stringify;
