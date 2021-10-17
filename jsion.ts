2ec9cef8cf86daad4111d5685a93157ed0a8d01f
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-10-16 18:22:46 -0600

    Fixed issues with number literals, null, and t/f

--- jsion.ts
+++ jsion.ts
@@ -1,7 +1,10 @@
 namespace JSION {
-    const COMMENT_PATTERN = /(?<=\\"|"(?:\\"|[^"])*")|(?:#(?:\\["\\\[\]{},:]|[^"\\\n\[\]{},:])*)/gm;
+    const COMMENT_PATTERN = /\\"|"(?:\\"|[^"])*"|(#[\s\S]*?)(?=(?<!\\|\\\d+)(?:true|false|null|\d+|[",:{}[\]]))/g;
     export function parse(text: string, reviver?: ((this: any, key: string, value: any) => any) | undefined): any {
-        return JSON.parse(text.replace(COMMENT_PATTERN, ''), reviver);
+        return JSON.parse(text.replace(COMMENT_PATTERN, function(substring: string, ...args: any[]) {
+            if(!args[0]) return substring
+            return ''
+        }), reviver);
     }
     export let stringify = JSON.stringify;
 }
