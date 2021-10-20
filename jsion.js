ee8ce256773e5c091a00de75b2c20431c3791383
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-10-19 18:44:32 -0600

    Complete overhaul

--- jsion.js
+++ jsion.js
@@ -1,9 +1,17 @@
 "use strict";
 var JSION;
 (function (JSION) {
-    const COMMENT_PATTERN = /\\"|"(?:\\"|[^"])*"|(#[\s\S]*?)(?=(?<!\\|\\\d+)(?:true|false|null|\d+|[",:{}[\]]))/g;
+    const COMMENT_PATTERN = /\\"|"(?:\\"|[^"])*"|(\([\S\s]*?(?<!\\)(?:\\\\)*\))/g, KEY_PATTERN = /\\"|"(?:\\"|[^"])*"|(\w*)(?=\s*?:)/g, TRAILING_COMMA_PATTERN = /\\"|"(?:\\"|[^"])*"|(,)(?=\s*?[}\]])/g;
     function parse(text, reviver) {
         return JSON.parse(text.replace(COMMENT_PATTERN, function (substring, ...args) {
+            if (!args[0])
+                return substring;
+            return '';
+        }).replace(KEY_PATTERN, function (substring, ...args) {
+            if (!args[0])
+                return substring;
+            return `"${args[0]}"`;
+        }).replace(TRAILING_COMMA_PATTERN, function (substring, ...args) {
             if (!args[0])
                 return substring;
             return '';
