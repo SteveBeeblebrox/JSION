3f0ed6a93c8eceba96140e5720562d410243bae7
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-10-19 18:43:32 -0600

    Complete overhaul

--- jsion.ts
+++ jsion.ts
@@ -1,7 +1,16 @@
 namespace JSION {
-    const COMMENT_PATTERN = /\\"|"(?:\\"|[^"])*"|(#[\s\S]*?)(?=(?<!\\|\\\d+)(?:true|false|null|\d+|[",:{}[\]]))/g;
+    const
+        COMMENT_PATTERN = /\\"|"(?:\\"|[^"])*"|(\([\S\s]*?(?<!\\)(?:\\\\)*\))/g,
+        KEY_PATTERN = /\\"|"(?:\\"|[^"])*"|(\w*)(?=\s*?:)/g,
+        TRAILING_COMMA_PATTERN = /\\"|"(?:\\"|[^"])*"|(,)(?=\s*?[}\]])/g;
     export function parse(text: string, reviver?: ((this: any, key: string, value: any) => any) | undefined): any {
         return JSON.parse(text.replace(COMMENT_PATTERN, function(substring: string, ...args: any[]) {
+            if(!args[0]) return substring
+            return ''
+        }).replace(KEY_PATTERN, function(substring: string, ...args: any[]) {
+            if(!args[0]) return substring
+            return `"${args[0]}"`
+        }).replace(TRAILING_COMMA_PATTERN, function(substring: string, ...args: any[]) {
             if(!args[0]) return substring
             return ''
         }), reviver);
