d856d9b8b1e6993850b1a706e690a3086f4497a3
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-10-23 11:56:55 -0600

    Bug fixes & single quoted support

--- jsion.ts
+++ jsion.ts
@@ -1,10 +1,14 @@
 namespace JSION {
     const
-        COMMENT_PATTERN = /\\"|"(?:\\"|[^"])*"|(\([\S\s]*?(?<!\\)(?:\\\\)*\))/g,
-        KEY_PATTERN = /\\"|"(?:\\"|[^"])*"|(\w*)(?=\s*?:)/g,
-        TRAILING_COMMA_PATTERN = /\\"|"(?:\\"|[^"])*"|(,)(?=\s*?[}\]])/g;
+        SINGLE_QUOTE_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|'([\S\s]*?(?<!\\)(?:\\\\)*)'/g,
+        COMMENT_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(\([\S\s]*?(?<!\\)(?:\\\\)*\))/g,
+        KEY_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(\w*)(?=\s*?:)/g,
+        TRAILING_COMMA_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(,)(?=\s*?[}\]])/g;
     export function parse(text: string, reviver?: ((this: any, key: string, value: any) => any) | undefined): any {
-        return JSON.parse(text.replace(COMMENT_PATTERN, function(substring: string, ...args: any[]) {
+        return JSON.parse(text.replace(SINGLE_QUOTE_PATTERN, function(substring: string, ...args: any[]) {
+            if(!args[0]) return substring
+            return `"${args[0].replace(/"/g, '\\"').replace(/\\'/g, "'")}"`
+        }).replace(COMMENT_PATTERN, function(substring: string, ...args: any[]) {
             if(!args[0]) return substring
             return ''
         }).replace(KEY_PATTERN, function(substring: string, ...args: any[]) {
