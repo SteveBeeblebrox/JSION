1394ceaf075a6ce1ea57b52d53474be54ccd513c
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-11-05 16:39:07 -0600

    Changes to implicit nulls and null shorthand

--- jsion.ts
+++ jsion.ts
@@ -4,8 +4,8 @@ namespace JSION {
         COMMENT_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(\([\S\s]*?(?<!\\)(?:\\\\)*\))/g,
         KEY_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|([a-zA-Z_$][0-9a-zA-Z_$]*)(?=\s*?:)/g,
         TRAILING_COMMA_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(,)(?=\s*?[}\]])/g,
-        ARRAY = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|((?<!\\)(?:\\{2})*\[(?:(?<!\\)(?:\\{2})*\\\]|[^\]])+(?<!\\)(?:\\{2})*\])/g,
         EMPTY_ARRAY_ITEM = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(?<=\[|,)()(?=[,\]])/g,
+        NULL_SHORTHAND_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(\?)/g,
         NUMBER_SEPERATOR = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(?<=\d)(_)(?=\d)/g;
     export function parse(text: string, reviver?: ((this: any, key: string, value: any) => any) | undefined): any {
         return JSON.parse(text.replace(SINGLE_QUOTE_PATTERN, function(substring: string, ...args: any[]) {
@@ -20,12 +20,12 @@ namespace JSION {
         }).replace(TRAILING_COMMA_PATTERN, function(substring: string, ...args: any[]) {
             if(!args[0]) return substring
             return ''
-        }).replace(ARRAY, function(substring: string, ...args: any[]) {
+        }).replace(EMPTY_ARRAY_ITEM, function(substring: string, ...args: any[]) {
+            if(args[0] === undefined) return substring
+            return 'null'
+        }).replace(NULL_SHORTHAND_PATTERN, function(substring: string, ...args: any[]) {
             if(!args[0]) return substring
-            return args[0].replace(EMPTY_ARRAY_ITEM, function(substring: string, ...args: any[]) {
-                if(args[0] === undefined) return substring
-                return 'null'
-            })
+            return 'null'
         }).replace(NUMBER_SEPERATOR, function(substring: string, ...args: any[]) {
             if(!args[0]) return substring
             return ''
