0f622a7d0851dd9f078b0c979099ff8176bb60bb
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-11-10 20:28:12 -0700

    Update jsion.ts

--- jsion.ts
+++ jsion.ts
@@ -4,7 +4,8 @@ namespace JSION {
         COMMENT_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(\([\S\s]*?(?<!\\)(?:\\\\)*\))/g,
         KEY_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|([a-zA-Z_$][0-9a-zA-Z_$]*)(?=\s*?:)/g,
         TRAILING_COMMA_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(,)(?=\s*?[}\]])/g,
-        EMPTY_ARRAY_ITEM = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(?<=[\[,])()(?=[,\]])/g,
+        EMPTY_ARRAY_ITEM = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(?<=[\[,])(\s*?)(?=[,\]])/g,
+        EMPTY_OBJECT_ITEM = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(?<=:)(\s*?)(?=[,}])/g,
         NULL_SHORTHAND_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(\?)/g,
         NUMBER_SEPERATOR = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])+(?<!\\)(?:\\{2})*"|(?<=\d)(_)(?=\d)/g;
     export function parse(text: string, reviver?: ((this: any, key: string, value: any) => any) | undefined): any {
@@ -21,6 +22,9 @@ namespace JSION {
             if(!args[0]) return substring
             return ''
         }).replace(EMPTY_ARRAY_ITEM, function(substring: string, ...args: any[]) {
+            if(args[0] === undefined) return substring
+            return 'null'
+        }).replace(EMPTY_OBJECT_ITEM, function(substring: string, ...args: any[]) {
             if(args[0] === undefined) return substring
             return 'null'
         }).replace(NULL_SHORTHAND_PATTERN, function(substring: string, ...args: any[]) {
