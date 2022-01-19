9347fdfb594bccb8a65504495990b2c0914904bc
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2022-01-18 19:49:23 -0700

    Bug fixes

--- jsion.ts
+++ jsion.ts
@@ -1,7 +1,7 @@
 namespace JSION {
     const
+        COMMENT_PATTERN = /(?<!\\)(?:\\{2})*'(?:(?<!\\)(?:\\{2})*\\'|[^'])*(?<!\\)(?:\\{2})*'|(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(\([\S\s]*?(?<!\\)(?:\\\\)*\))/g,
         SINGLE_QUOTE_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|'([\S\s]*?(?<!\\)(?:\\\\)*)'/g,
-        COMMENT_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(\([\S\s]*?(?<!\\)(?:\\\\)*\))/g,
         KEY_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|([a-zA-Z_$][0-9a-zA-Z_$]*)(?=\s*?:)/g,
         TRAILING_COMMA_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(,)(?=\s*?[}\]])/g,
         EMPTY_ARRAY_ITEM = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(?<=[\[,])(\s*?)(?=[,\]])/g,
@@ -9,13 +9,12 @@ namespace JSION {
         NULL_SHORTHAND_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(\?)/g,
         NUMBER_SEPERATOR = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(?<=\d)(_)(?=\d)/g;
     export function parse(text: string, reviver?: ((this: any, key: string, value: any) => any) | undefined): any {
-        return console.log(text.replace(SINGLE_QUOTE_PATTERN, function(substring: string, ...args: any[]) {
-            if(args[0] === undefined) return substring
-            console.log(args[0])
-            return `"${args[0].replace(/"/g, '\\"').replace(/\\'/g, "'")}"`
-        }).replace(COMMENT_PATTERN, function(substring: string, ...args: any[]) {
+        return JSON.parse(text.replace(COMMENT_PATTERN, function(substring: string, ...args: any[]) {
             if(!args[0]) return substring
             return ''
+        }).replace(SINGLE_QUOTE_PATTERN, function(substring: string, ...args: any[]) {
+            if(args[0] === undefined) return substring
+            return `"${args[0].replace(/"/g, '\\"').replace(/\\'/g, "'")}"`
         }).replace(KEY_PATTERN, function(substring: string, ...args: any[]) {
             if(!args[0]) return substring
             return `"${args[0]}"`
