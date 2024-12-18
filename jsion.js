48d9a80eaeaf22c1ac083d9dfe28675c5532c5d3
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-12-18 12:52:36 -0700

    Update jsion.js

--- jsion.js
+++ jsion.js
@@ -1,42 +1,46 @@
 "use strict";
 var JSION;
 (function (JSION) {
-    const COMMENT_PATTERN = /(?<!\\)(?:\\{2})*'(?:(?<!\\)(?:\\{2})*\\'|[^'])*(?<!\\)(?:\\{2})*'|(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(\([\S\s]*?(?<!\\)(?:\\\\)*\))/g, SINGLE_QUOTE_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|'([\S\s]*?(?<!\\)(?:\\\\)*)'/g, KEY_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|([a-zA-Z_$][0-9a-zA-Z_$]*)(?=\s*?:)/g, TRAILING_COMMA_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(,)(?=\s*?[}\]])/g, EMPTY_ARRAY_ITEM = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(?<=[\[,])(\s*?)(?=[,\]])/g, EMPTY_OBJECT_ITEM = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(?<=:)(\s*?)(?=[,}])/g, NULL_SHORTHAND_PATTERN = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(\?)/g, NUMBER_SEPERATOR = /(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"|(?<=\d)(_)(?=\d)/g;
-    function parse(text, reviver) {
-        return JSON.parse(text.replace(COMMENT_PATTERN, function (substring, ...args) {
-            if (!args[0])
-                return substring;
-            return '';
-        }).replace(SINGLE_QUOTE_PATTERN, function (substring, ...args) {
-            if (args[0] === undefined)
-                return substring;
-            return `"${args[0].replace(/"/g, '\\"').replace(/\\'/g, "'")}"`;
-        }).replace(KEY_PATTERN, function (substring, ...args) {
-            if (!args[0])
-                return substring;
-            return `"${args[0]}"`;
-        }).replace(TRAILING_COMMA_PATTERN, function (substring, ...args) {
-            if (!args[0])
-                return substring;
-            return '';
-        }).replace(EMPTY_ARRAY_ITEM, function (substring, ...args) {
-            if (args[0] === undefined)
-                return substring;
-            return 'null';
-        }).replace(EMPTY_OBJECT_ITEM, function (substring, ...args) {
-            if (args[0] === undefined)
-                return substring;
-            return 'null';
-        }).replace(NULL_SHORTHAND_PATTERN, function (substring, ...args) {
-            if (!args[0])
-                return substring;
-            return 'null';
-        }).replace(NUMBER_SEPERATOR, function (substring, ...args) {
-            if (!args[0])
-                return substring;
-            return '';
-        }), reviver);
-    }
-    JSION.parse = parse;
+    JSION.VERSION = Object.freeze({
+        toString() { return `${JSION.VERSION.major}.${JSION.VERSION.minor}.${JSION.VERSION.patch}${JSION.VERSION.prerelease !== undefined ? `-${JSION.VERSION.prerelease}` : ''}${JSION.VERSION.metadata !== undefined ? `+${JSION.VERSION.metadata}` : ''}`; },
+        major: 1, minor: 0, patch: 1
+    });
+    JSION.transpile = function (text) {
+        // Character escapes, streaming?
+        return text.replace(...compile({
+            // Double quote strings
+            [/(?<!\\)(?:\\{2})*"(?:(?<!\\)(?:\\{2})*\\"|[^"])*(?<!\\)(?:\\{2})*"/.source]: text => text,
+            // Single quote strings
+            [/(?<!\\)(?:\\{2})*'(?:(?<!\\)(?:\\{2})*\\'|[^'])*(?<!\\)(?:\\{2})*'/.source]: text => `"${text.slice(1, -1).replace(/"/g, '\\"').replace(/\\'/g, "'")}"`,
+            // Comments
+            [/\(\*[\s\S]*?(?<!\\)(?:\\\\)*\*\)/.source]: () => '',
+            // Unquoted keys
+            [/[a-zA-Z_$][0-9a-zA-Z_$]*(?=\s*?:)/.source]: text => `"${text}"`,
+            // Trailing commas
+            [/,(?=\s*?[}\]])/.source]: () => '',
+            // Empty array items
+            [/(?<=\[)(\s*?)(?=,)|(?<=,)(\s*?)(?=[,\]])/.source]: text => text + 'null',
+            // Empty object items
+            [/(?<=:)(\s*?)(?=[,}])/.source]: text => text + 'null',
+            // Null shorthand
+            [/\?+/.source]: () => 'null',
+            // Hex literals, octal literals, and binary literals
+            [/\b(?:0b(?:[0-1](?:_[0-1])*?)+|0o(?:[0-7](?:_[0-7])*?)+|0x(?:[0-9a-fA-F](?:_[0-9a-fA-F])*?)+)\b/.source]: text => +text.replace(/_/g, '') + '',
+            // Numeric seperators
+            [/(?<=\d)_(?=\d)/.source]: () => ''
+        }, 'g')).replace(/\n([^\S\n]*\n)+/g, '\n').trim();
+    };
+    JSION.parse = function parse(text, ...args) {
+        return JSON.parse(JSION.transpile(text), ...args);
+    };
     JSION.stringify = JSON.stringify;
+    function compile(patterns, flags) {
+        return [
+            new RegExp(Object.keys(patterns).map((pattern, i) => `(?<_${i}>${pattern})`).join('|'), flags),
+            function (...args) {
+                const [i, text] = Object.entries(args.at(-1)).find(([, text]) => text != null);
+                return Object.values(patterns)[+i.slice(1)](text);
+            }
+        ];
+    }
 })(JSION || (JSION = {}));
